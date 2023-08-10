d84551c185250ad822a92cbf4753fdb6fc45003b
ZubairSharif21 <zubair918sharif@gmail.com>
2023-08-10 06:10:50 -0700

    Payment Success But not to shown

---
+++ resources/views/payment_result.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Result</title>
+</head>
+<body>
+    <h1>Payment Result</h1>
+
+
+    <p>Transaction ID: {{ $transactionId }}</p>
+    <p>Amount: {{ $amount }}</p>
+
+    <a href="/">Go back to the homepage</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\PaymentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('pay-card',[PaymentController::class,'pay_card'])->name('pay');
+
+
+Route::get('/payment', [PaymentController::class, 'showPaymentForm'])->name('payment.form');
+Route::post('/payment/process', [PaymentController::class, 'processPayment'])->name('payment.process');
